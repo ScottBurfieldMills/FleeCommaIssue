912c334cc44f7322014c8752870cd75248d8c0cb
Scott Burfield-Mills <scott@firstsoftware.co.nz>
2020-09-16 11:06:42 +1200

    All the things

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Flee.PublicTypes;
+
+namespace ExpressionPlayground
+{
+    public static class CustomFunctions
+    {
+        // Declare a function that takes a variable number of arguments
+        public static int Sum(params int[] args)
+        {
+            int sum = 0;
+
+            foreach (int i in args)
+                sum += i;
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionContext context = new ExpressionContext();
+            context.Imports.AddType(typeof(CustomFunctions));
+
+            IDynamicExpression e = context.CompileDynamic("sum(1,2,3,4,5,6)");
+            int result = (int)e.Evaluate();
+
+            Console.WriteLine("result: " + result);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
